3611e508bd37559d4d1fc063f2372f2fc2728c6e
Ranit Jana <ranitjana100@gmail.com>
2024-01-31 19:32:27 +0530

    add priority queue

--- queue.cpp
+++ queue.cpp
@@ -199,23 +199,93 @@ public:
     }
 };
 
+// priority queue using linked list
+class PriorityQueue
+{
+    struct Node
+    {
+        int data, priority;
+        Node *next;
+        Node(int value, int prio)
+        {
+            data = value;
+            priority = prio;
+            next = NULL;
+        }
+    } *head;
+
+public:
+    PriorityQueue()
+    {
+        head = NULL;
+    }
+    void insert(int value, int prio)
+    {
+        Node *newNode = new Node(value, prio);
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else if (head->priority < prio)
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next && temp->next->priority >= prio)
+                temp = temp->next;
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+    }
+    int erase()
+    {
+        if (!head)
+        {
+            cout << "Empty." << endl;
+            return -1;
+        }
+        int value = head->data;
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return value;
+    }
+    void display()
+    {
+        if (head == NULL)
+        {
+            cout << "Empty." << endl;
+            return;
+        }
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->data << " " << temp->priority << endl;
+            temp = temp->next;
+        }
+    }
+};
+
 int main()
 {
-    CircularQueue q(5);
+    PriorityQueue q;
     while (1)
     {
         int value;
         cin >> value;
         if (value == 1)
         {
-            cout << "Enter data";
-            int data;
-            cin >> data;
-            q.enqueue(data);
+            cout << "Enter data and prio\n";
+            int data, p;
+            cin >> data >> p;
+            q.insert(data, p);
         }
         else
         {
-            cout << q.dequeue() << endl;
+            cout << q.erase() << endl;
         }
         q.display();
     }
